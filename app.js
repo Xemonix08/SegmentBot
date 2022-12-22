381a57afe64c58700a8539f3bb98367a509823fc
Anananel <dmitrenko.ivan08@gmail.com>
2022-12-22 12:33:44 +0200

    small tweaks

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-const { Client, GatewayIntentBits, WebhookClient, EmbedBuilder } = require('discord.js');
-const { token, logsWebhookUrl } = require('./config.json');
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 const { loadEventFiles, loadCommandFiles } = require('./getFiles.js');
-
-String.prototype.truncate = String.prototype.truncate || function (n) {
-    return (this.length > n) ? this.slice(0, n - 3) + '...' : this.toString();
-};
-
-console.error = (data) => {
-    const name = data.name ?? '-';
-    const message = data.message ?? data;
-    const stack = data.stack ?? '-';
-
-    try {
-        if (logsWebhookUrl !== '') {
-            const webhookClient = new WebhookClient({ url: logsWebhookUrl });
-
-            const embed = new EmbedBuilder()
-                .setAuthor({ name: 'Error', iconURL: 'https://raw.githubusercontent.com/abrahammurciano/discord-lumberjack/main/images/error.png' })
-                .setDescription(name)
-                .addFields([
-                    { name: 'Message', value: message.truncate(1024), inline: true },
-                    { name: 'Stack', value: stack.truncate(1024), inline: true }
-                ])
-                .setColor(0xFF0000);
-            webhookClient.send({ embeds: [embed] });
-        }
-    } catch (e) {
-        process.stdout.write('❌ | Unable to redirect error to the webhook\n');
-        console.log(e);
-    }
-    process.stdout.write(`[ERROR] ${name} : ${message}\n`);
-};
+require('./helpers.js');
 
 console.log('Starting the bot.');
 
--- helpers.js
+++ helpers.js
@@ -1,4 +1,37 @@
+const { WebhookClient, EmbedBuilder } = require('discord.js');
 const { User } = require('./database.js');
+const { logsWebhookUrl } = require('./config.json');
+
+String.prototype.truncate = String.prototype.truncate || function (n) {
+    return (this.length > n) ? this.slice(0, n - 3) + '...' : this.toString();
+};
+
+console.error = (data) => {
+    const name = data.name ?? '-';
+    const message = data.message ?? data;
+    const stack = data.stack ?? '-';
+
+    try {
+        if (logsWebhookUrl !== '') {
+            const webhookClient = new WebhookClient({ url: logsWebhookUrl });
+
+            const embed = new EmbedBuilder()
+                .setAuthor({ name: 'Error', iconURL: 'https://raw.githubusercontent.com/abrahammurciano/discord-lumberjack/main/images/error.png' })
+                .setDescription(name)
+                .addFields([
+                    { name: 'Message', value: message.truncate(1024), inline: true },
+                    { name: 'Stack', value: stack.truncate(1024), inline: true }
+                ])
+                .setColor(0xFF0000);
+            webhookClient.send({ embeds: [embed] });
+        }
+    } catch (e) {
+        process.stdout.write('❌ | Unable to redirect error to the webhook\n');
+        console.log(e);
+    }
+    process.stdout.write(`[ERROR] ${name} : ${message}\n`);
+    process.stdout.write(stack);
+};
 
 module.exports = {
     async getUser(id) {
